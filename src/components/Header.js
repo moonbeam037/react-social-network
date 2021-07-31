7e6a9922be9b2cce2d491f58193ff32df34aa1e3
moonbeam037 <iwannadie103@gmail.com>
2021-07-31 12:05:37 +0300

    add all files

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="" >АЙНУРИК</div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3" className="avatar"/>
+          ava + description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
